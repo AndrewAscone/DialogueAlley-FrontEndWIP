a9691f06a258eb4612e05a6636c8ba402f0b0a4c
AndrewAscone <andrew.ascone@gmail.com>
2023-01-11 15:59:01 -0500

    Created PostTable component, will be used for displaying posts from database

---
+++ src/components/PostTable.jsx
@@ -0,0 +1,34 @@
+import Table from 'react-bootstrap/Table';
+
+function PostTable() {
+  return (
+    <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Username</th>
+          <th>Message</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Andrew</td>
+          <td>@Scone527</td>
+          <td>This is a test!</td>
+        </tr>
+        <tr>
+          <td>Calvin</td>
+          <td>@Apex</td>
+          <td>Test message</td>
+        </tr>
+        <tr>
+            <td>John</td>
+            <td>@Doc</td>
+            <td>Another test</td>
+        </tr>
+      </tbody>
+    </Table>
+  );
+}
+
+export default PostTable;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,37 +1,10 @@
 import React from 'react'
+import PostTable from '../components/PostTable'
 
 export default function Home() {
   return (
     <div>
-        <table class="table table-bordered table-light table-striped table-hover">
-            <tbody>
-                <tr>
-                    <td>Name</td>
-                    <td>Handle</td>
-                    <td>Message</td>
-                </tr>
-                <tr>
-                    <td>Andrew</td>
-                    <td>Scone</td>
-                    <td>This is a test!</td>
-                </tr>
-                <tr>
-                    <td>Calvin</td>
-                    <td>Apex</td>
-                    <td>Test message</td>
-                </tr>
-                <tr>
-                    <td>John</td>
-                    <td>Doc</td>
-                    <td>Another test</td>
-                </tr>
-                <tr>
-                    <td>Test</td>
-                    <td>Test</td>
-                    <td>Test again</td>
-                </tr>
-            </tbody>
-        </table>
+        <PostTable/>
     </div>
   )
 }
